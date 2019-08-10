21e8da86959cec4c48f52536d1486aff10481311
Joshua Cole <joshuacole356@gmail.com>
2019-08-10 15:08:19 -0700

    return jwt from api login

--- routes/api/login.js
+++ routes/api/login.js
@@ -1,16 +1,26 @@
-// TODO: this is the future use JWT strategy
-
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const passport = require('passport');
+
+// TODO: this is the future use JWT strategy
+// TODO: create a secret key
+const createToken = id => jwt.sign({ id }, 'shhhhh');
 
 const router = express.Router();
 
 /* POST log in user */
 router.post(
   '/',
-  passport.authenticate('local'),
-  (_req, res) => {
-    res.send('You have been authenticated');
+  (req, res) => {
+    passport.authenticate('local', { failureRedirect: '/login', session: false }, (_err, user) => {
+      console.log('response', res);
+      const userId = user.getDataValue('id');
+      console.log('user', userId);
+      res.send({
+        msg: 'Successful login',
+        token: createToken(userId),
+      });
+    })(req, res);
   },
 );
 
