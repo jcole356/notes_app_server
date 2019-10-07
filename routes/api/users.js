e7bfffeb9c5d1688d7d8050cd68b72414d98b0b8
Joshua Cole <joshuacole356@gmail.com>
2019-10-06 23:38:13 -0700

    add create note endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -37,4 +37,41 @@ router.get(
   },
 );
 
+/* POST create note for user */
+router.post(
+  '/:userId/notes',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { body, params: { userId: userIdParam }, user: reqUser } = req;
+    const userId = reqUser.getDataValue('id');
+    if (userIdParam !== 'current' && userId !== parseInt(userIdParam, 10)) {
+      res.format({
+        'application/json': () => {
+          res.send('Not authorized');
+        },
+      });
+
+      return;
+    }
+    // TODO: need to verify the client sends the data correctly
+    models.Note.create(body).then((n) => {
+      reqUser.addNote(n).then(() => {
+        const user = models.User.findOne({
+          where: {
+            id: userId,
+          },
+          include: [models.Note],
+        });
+        user.then((u) => {
+          res.format({
+            'application/json': () => {
+              res.send({ notes: u.notes });
+            },
+          });
+        });
+      });
+    });
+  },
+);
+
 module.exports = router;
