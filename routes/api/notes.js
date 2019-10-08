50cc459a5d801ed548830bf822ca15a85d3ec975
Joshua Cole <joshuacole356@gmail.com>
2019-10-07 21:44:52 -0700

    add edit note endpoint

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -38,4 +38,38 @@ router.delete(
   },
 );
 
+/* PUT edit note by id */
+router.put(
+  '/:noteId',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { body, params: { noteId }, user: reqUser } = req;
+    const userId = reqUser.getDataValue('id');
+    const note = models.Note.findOne({
+      where: {
+        id: noteId,
+      },
+      include: [models.User],
+    });
+    note.then((n) => {
+      if (n.user.id !== userId) {
+        res.format({
+          'application/json': () => {
+            res.send('Not authorized');
+          },
+        });
+
+        return;
+      }
+      const updatedNote = { ...n, ...body };
+      n.update(updatedNote);
+      res.format({
+        'application/json': () => {
+          res.send({ note: n });
+        },
+      });
+    });
+  },
+);
+
 module.exports = router;
