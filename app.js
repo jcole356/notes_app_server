42a44c51ad02ca6d6533edd172ff721be7f77ac2
Joshua Cole <joshuacole356@gmail.com>
2020-03-29 21:06:10 -0700

    Match prettier with airbnb options

--- app.js
+++ app.js
@@ -25,25 +25,28 @@ opts.secretOrKey = 'shhhhh';
 // CORS?
 // opts.issuer = 'accounts.examplesoft.com';
 // opts.audience = 'yoursite.net';
-passport.use(new JWTStrategy(opts, (jwtPayload, cb) => {
-  models.User.findOne({
-    where: {
-      id: jwtPayload.id,
-    },
-  }).then((user) => {
-    if (!user) {
-      console.log('user not found (auth)');
-      cb(null, false);
-      return null;
-    }
-    console.log('successfully authenticated');
-    cb(null, user);
-    return null;
-  })
-    .catch((err) => {
-      console.log('error', err);
-    });
-}));
+passport.use(
+  new JWTStrategy(opts, (jwtPayload, cb) => {
+    models.User.findOne({
+      where: {
+        id: jwtPayload.id,
+      },
+    })
+      .then((user) => {
+        if (!user) {
+          console.log('user not found (auth)');
+          cb(null, false);
+          return null;
+        }
+        console.log('successfully authenticated');
+        cb(null, user);
+        return null;
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+  }),
+);
 
 // Demo code https://github.com/passport/express-4.x-local-example/blob/master/server.js
 // Configure the local strategy for use by Passport.
@@ -52,32 +55,34 @@ passport.use(new JWTStrategy(opts, (jwtPayload, cb) => {
 // (`username` and `password`) submitted by the user.  The function must verify
 // that the password is correct and then invoke `cb` with a user object, which
 // will be set at `req.user` in route handlers after authentication.
-passport.use(new LocalStrategy({ session: false },
-  (username, password, cb) => {
+passport.use(
+  new LocalStrategy({ session: false }, (username, password, cb) => {
     models.User.findAll({
       where: {
         username,
       },
-    }).then((users) => {
-      if (!users || users.length < 1) {
-        console.log('no user');
-        return cb(null, false);
-      }
-      const user = users[0];
-      const passwordDigest = user.getDataValue('passwordDigest');
-      return validatePassword(password, passwordDigest).then((same) => {
-        if (same) {
-          console.log('successfully authenticated');
-          return cb(null, user);
-        }
-        console.log('password wrong');
-        return cb(null, false);
-      });
     })
+      .then((users) => {
+        if (!users || users.length < 1) {
+          console.log('no user');
+          return cb(null, false);
+        }
+        const user = users[0];
+        const passwordDigest = user.getDataValue('passwordDigest');
+        return validatePassword(password, passwordDigest).then((same) => {
+          if (same) {
+            console.log('successfully authenticated');
+            return cb(null, user);
+          }
+          console.log('password wrong');
+          return cb(null, false);
+        });
+      })
       .catch((err) => {
         console.log('error', err);
       });
-  }));
+  }),
+);
 
 const app = express();
 
@@ -90,11 +95,13 @@ app.use(bodyParser.json());
 
 // TODO: remove?
 app.use(cookieParser());
-app.use(session({
-  secret: 'cats suck',
-  resave: false,
-  saveUninitialized: true,
-}));
+app.use(
+  session({
+    secret: 'cats suck',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
 
 // TODO: configure cors per api
 // Enable cors requests
