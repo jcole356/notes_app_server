2f448aabb902334d0b98a012d3258c7f97186371
Joshua Cole <joshuacole356@gmail.com>
2021-02-26 16:16:24 -0800

    Create src directory

--- bin/www.js
+++ bin/www.js
@@ -7,8 +7,8 @@ import Debug from 'debug';
 import dotenv from 'dotenv/config'; // eslint-disable-line no-unused-vars
 import http from 'http';
 
-import app from '../app';
-import { sequelize } from '../models';
+import app from '../src/app';
+import { sequelize } from '../src/models';
 
 const debug = Debug('http');
 
