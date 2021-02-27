a1452e173abfe9c4deffeeeaaa26b261d9ba17e5
Joshua Cole <joshuacole356@gmail.com>
2021-02-26 17:04:12 -0800

    Use database url to config Sequelize in production

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,19 @@
 import Sequelize from 'sequelize';
 
-const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  process.env.DATABASE_PASSWORD,
-  {
-    dialect: 'postgres',
-  },
-);
+let sequelize; // eslint-disable-line import/no-mutable-exports
+
+if (process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DATABASE,
+    process.env.DATABASE_USER,
+    process.env.DATABASE_PASSWORD,
+    {
+      dialect: 'postgres',
+    },
+  );
+}
 
 const models = {
   Note: sequelize.import('./note'),
