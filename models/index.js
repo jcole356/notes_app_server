3edbcf53badd86353736c642d9a6659e49b1e247
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2021-10-07 19:30:26 -0700

    Add migration for users table

--- models/index.js
+++ models/index.js
@@ -25,6 +25,7 @@ fs
     db[model.name] = model;
   });
 
+// TODO: case sensitivity
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
---
+++ models/user.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      models.User.hasMany(models.Note, { foreignKey: 'userId' });
+    }
+  };
+
+  User.init({
+    username: DataTypes.STRING,
+    passwordDigest: DataTypes.STRING,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+
+  return User;
+};
