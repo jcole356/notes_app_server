3ca1b2c96471d373bdb682d22627d871a7129062
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2021-10-08 23:36:22 -0700

    Seed a test user

---
+++ seeders/20211009061220-test-user.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const passwordDigest = await bcrypt.hash('password', 10);
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          username: 'testy',
+          email: 'test@test.com',
+          passwordDigest,
+          createdAt: new Date(),
+          updatedAt: new Date()
+        },
+      ],
+      {},
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
