435003eebc5950efd8d28abde971549297401aee
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2021-10-09 12:17:55 -0700

    Change migrations to use uppercase table names

--- migrations/20210301010339-create-note.js
+++ migrations/20210301010339-create-note.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('notes', {
+    await queryInterface.createTable('Notes', {
       id: {
         allowNull: false,
         autoIncrement: true,
