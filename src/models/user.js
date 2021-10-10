8691bfff8ea9c701a0ba805c8e300548cf9003c1
Joshua Cole <9787792+jcole356@users.noreply.github.com>
2021-10-09 23:49:54 -0700

    Fix linting

--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,23 @@
-'use strict';
-
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate(models) {
       models.User.hasMany(models.Note, { foreignKey: 'userId' });
     }
-  };
+  }
 
-  User.init({
-    username: DataTypes.STRING,
-    passwordDigest: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    {
+      username: DataTypes.STRING,
+      passwordDigest: DataTypes.STRING,
+      email: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: 'User',
+    },
+  );
 
   return User;
 };
